11c63d1e094a650e6bce342ff61072e1052c66e3
Kongbaimoon <1216855441@qq.com>
2023-06-30 11:22:36 +0800

    conflict molecule.py

--- python/spider/molecule.py
+++ python/spider/molecule.py
@@ -184,7 +184,7 @@ def get_molecule_info(id, authorization_token):
         
 
 def mole_info():
-    authorization_token = ""
+    authorization_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhY2Nlc3NUb2tlbiIsImNyZWF0ZWRBdCI6IjE2ODc5NTc4OTg5NTgiLCJleHBpcmVkQXQiOiIxNjg3OTY1MDk4OTU4Iiwicm9sZSI6IlJPTEVfVVNFUiIsImp0aSI6InBlcnNldXMiLCJlbWFpbCI6Ijk3MjMxMzAwNkBxcS5jb20ifQ.QpW8DA7Ayjr80GVE62-MGLV702WULeHPtr_GY5ek8Xg"
     
     data_dict = {
         "id": [],
@@ -193,13 +193,17 @@ def mole_info():
     
     with open("data/mol_ids.txt", "r") as f:
         lines = f.readlines()
-        
+        i = 0 
         ids = []
         for line in lines:
             id = line.strip()
             data, authorization_token = get_molecule_info(id, authorization_token)
             data_dict["id"].append(id)
             data_dict["data"].append(data)
+
+            i += 1
+            if i % 100 == 0:
+                print(i)
     
     df = pd.DataFrame(data_dict)
     df.to_pickle("data/mol_info.pickle")
@@ -231,3 +235,105 @@ print(r.text)
 
 time.sleep(1)
     
+# mole_info()
+
+import threading
+
+authorization_token = None
+
+def get_molecule_info2(id):
+    global authorization_token
+
+    url = "http://www.dcaiku.com/v1/perseus/mol_search"
+    url_get_token = "http://www.dcaiku.com/v1/public/token_refresh"
+    
+    headers = {
+        "Accept": "application/json, text/plain, */*",
+        "Accept-Encoding": "gzip, deflate",
+        "Accept-Language": "zh-CN,zh;q=0.9",
+        "Authorization": authorization_token,
+        "Content-Length": "36",
+        "Content-Type": "application/json;charset=UTF-8",
+        "Host": "www.dcaiku.com",
+        "Origin": "http://www.dcaiku.com",
+        "Proxy-Connection": "keep-alive",
+        "Referer": "http://www.dcaiku.com/",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+    }
+
+    data_json = [
+        {"label": id,"type":"id"}
+    ]
+    
+    refresh_json = {
+        "refreshToken": authorization_token
+    }
+    k_count = 0
+    
+    while True:
+        try:
+            r = requests.post(url, data=json.dumps(data_json), headers=headers)
+            data = json.loads(r.text)
+            
+            return data
+        except:
+            print(k_count)
+            r = requests.post(url_get_token, data=json.dumps(refresh_json), headers=headers)
+            token_json = json.loads(r.text)
+            if "error" in token_json.keys():
+                print(r.text)
+                break
+            authorization_token = token_json["message"]
+            continue
+    
+    return None
+
+class MyThread(threading.Thread):
+
+    def __init__(self, data, authorization_token, n):
+        self.data = data
+        # self.authorization_token = authorization_token
+        self.n = n
+        # 注意：一定要调用父类的初始化函数，否则否发创建线程
+        super().__init__()
+
+    def run(self) -> None:
+        data_dict = {
+            "id": [],
+            "data": []
+        }
+        
+        for line in self.data:
+            id = line.strip()
+            data_r = get_molecule_info2(id)
+            data_dict["id"].append(id)
+            data_dict["data"].append(data_r)
+
+        
+        df = pd.DataFrame(data_dict)
+        df.to_pickle("data/mol_info_" + str(self.n) + ".pickle")
+
+# authorization_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhY2Nlc3NUb2tlbiIsImNyZWF0ZWRBdCI6IjE2ODgwMjQyMTE4OTEiLCJleHBpcmVkQXQiOiIxNjg4MDMxNDExODkxIiwicm9sZSI6IlJPTEVfVVNFUiIsImp0aSI6InBlcnNldXMiLCJlbWFpbCI6InJhbGV2YTg5OTBAZml0d2wuY29tIn0.rHghTuXXC7ylsLZGeEFagnb_iJXqLqFwvF6wuk70moc"
+    
+# data_dict = {
+#     "id": [],
+#     "data": []
+# }
+
+# with open("data/mol_ids.txt", "r") as f:
+#     lines = f.readlines()
+
+# lens = int(len(lines) / 100)
+# for i in range(100):
+#     if i != 100:
+#         # t = MyThread(lines[i*lens:(i+1)*lens], authorization_token, i)
+#         t = MyThread(lines[i*10:(i+1)*10], authorization_token, i)
+#     # else:
+#     #     t = MyThread(lines[i*10:], authorization_token, i)
+
+#     t.start()
+
+path = "data/mol_info_80.pickle"
+df = pd.read_pickle(path)
+
+print(df)
